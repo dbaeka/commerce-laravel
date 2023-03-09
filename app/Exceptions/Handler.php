b2355e2b12cb169b8dbd1e624e90b0730d0ae8b8
Delmwin Baeka <dbaekajnr@gmail.com>
2023-03-09 19:42:05 +0000

    :art: Modify json response to include success field

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,13 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -41,8 +47,75 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function (Throwable $e, $request) {
+            if ($request->wantsJson()) {
+                $this->handleApiException($request, $e);
+            }
+        });
+
+    }
+
+    private function handleApiException(Request $request, Throwable|AuthenticationException|ValidationException $e): void
+    {
+        $exception = $this->prepareException($e);
+
+        if ($exception instanceof HttpResponseException) {
+            $exception = $exception->getResponse();
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            $exception = $this->unauthenticated($request, $e);
+        }
+
+        if ($exception instanceof ValidationException) {
+            $exception = $this->convertValidationExceptionToResponse($e, $request);
+        }
+
+        $this->customApiResponse($exception);
+    }
+
+    private function customApiResponse(Throwable|JsonResponse|Response $e): void
+    {
+        if (method_exists($e, 'getStatusCode')) {
+            $statusCode = $e->getStatusCode();
+        } else {
+            $statusCode = 500;
+        }
+
+        switch ($statusCode) {
+            case 401:
+                $response['message'] = 'Unauthorized';
+                break;
+            case 403:
+                $response['message'] = 'Forbidden';
+                break;
+            case 404:
+                $response['message'] = 'Not Found';
+                break;
+            case 405:
+                $response['message'] = 'Method Not Allowed';
+                break;
+            case 422:
+                $response['message'] = $e->original['message'];
+                $response['errors'] = $e->original['errors'];
+                break;
+            default:
+                $response['message'] = ($statusCode == 500) ? 'Whoops, looks like something went wrong' : $e->getMessage();
+                break;
+        }
+
+        if (config('app.debug') && $e instanceof Throwable) {
+            $response['trace'] = $e->getTrace();
+            $response['code'] = $e->getCode();
+        }
+
+        $response['success'] = false;
+
+        response()->json($response, $statusCode);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,17 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ProductResource;
+use App\Http\Resources\ProductCollection;
 use App\Services\ProductServiceInterface;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
-    public function index(Request $request, ProductServiceInterface $productService): AnonymousResourceCollection
+    public function index(Request $request, ProductServiceInterface $productService): ProductCollection
     {
         $products = $productService->getProducts();
 
-        return ProductResource::collection($products);
+        return new ProductCollection($products);
     }
 }
