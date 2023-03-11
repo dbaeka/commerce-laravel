f0a0937f8066d50d03f8c4876c660bad161ebb97
dbaeka <dbaekarjnr@gmail.com>
2023-03-11 22:15:52 +0000

    :construction: :sparkle: add caching to products to improve speed

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\DTO\ProductDTO;
 use Illuminate\Http\Client\HttpClientException;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class ProductService extends BaseYcodeService implements ProductServiceInterface
@@ -12,24 +13,45 @@ class ProductService extends BaseYcodeService implements ProductServiceInterface
 
     public function getProducts(): ?array
     {
+        $products = $this->getProductsFromCache() ?? $this->getProductsFromRemote();
+        if (empty($products)) {
+            return null;
+        }
+        $productDTOs = [];
+        foreach ($products as $product) {
+            $productDTOs[] = new ProductDTO(
+                external_id: $product["_ycode_id"],
+                name: $product["Name"],
+                price: $product["Price"],
+                slug: $product["Slug"],
+                image_url: $product["Image"],
+                color: $product["Color"]
+            );
+        }
+        return $productDTOs;
+    }
+
+    private function getProductsFromCache(): ?array
+    {
+        $products = Cache::get('products_list');
+        if (!empty($products)) {
+            return json_decode($products, true);
+        }
+        return null;
+    }
+
+    private function getProductsFromRemote(): ?array
+    {
         $this->validateEnvVariables();
         $request = $this->getBaseRequest();
         $collection_id = $this->getCollectionId();
         try {
             $response = $request->get("collections/$collection_id/items", []);
             $response->throw();
-            $productDTOs = [];
-            foreach ($response->json("data") as $product) {
-                $productDTOs[] = new ProductDTO(
-                    external_id: $product["_ycode_id"],
-                    name: $product["Name"],
-                    price: $product["Price"],
-                    slug: $product["Slug"],
-                    image_url: $product["Image"],
-                    color: $product["Color"]
-                );
-            }
-            return $productDTOs;
+            $data = $response->json('data');
+            // Cache results for 1 minute
+            Cache::set('products_list', json_encode($data), 60);
+            return $data;
         } catch (HttpClientException $e) {
             Log::error(__CLASS__ . ' ' . __FUNCTION__ . ': Error making the call: ' . $e->getMessage(), [
                 $e,
--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -4,11 +4,62 @@ namespace Tests\Unit;
 
 use App\DTO\ProductDTO;
 use App\Services\ProductServiceInterface;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Mockery;
 
 class ProductServiceTest extends YcodeServiceTestCase
 {
     protected string $collection_id = "products_id";
+
+    public function test_it_gets_products_from_remote_returns_product_dto()
+    {
+        $body = file_get_contents(base_path('tests/Fixtures/Services/GetProductsSampleResponse.json'));
+
+        Cache::shouldReceive('get')
+            ->once()
+            ->with('products_list')
+            ->andReturn(null);
+
+        Cache::shouldReceive('set')
+            ->once()
+            ->with('products_list', Mockery::any(), Mockery::type('int'))
+            ->andReturn(null);
+
+        Http::fake([
+            "$this->domain/*" => Http::response($body, 200),
+        ]);
+
+        $productDTOs = app(ProductServiceInterface::class)->getProducts();
+
+        $this->assertCount(2, $productDTOs);
+
+        $this->assertInstanceOf(
+            ProductDTO::class,
+            $productDTOs[0]
+        );
+    }
+
+
+    public function test_it_gets_products_from_cache_returns_product_dto()
+    {
+        $body = file_get_contents(base_path('tests/Fixtures/Services/GetProductsSampleResponse.json'));
+        $body = json_decode($body, true);
+        Cache::shouldReceive('get')
+            ->once()
+            ->with('products_list')
+            ->andReturn(json_encode($body["data"]));
+
+        $productDTOs = app(ProductServiceInterface::class)->getProducts();
+
+        $this->assertCount(2, $productDTOs);
+
+        $this->assertInstanceOf(
+            ProductDTO::class,
+            $productDTOs[0]
+        );
+    }
+
 
     public function test_it_gets_products_returns_product_dto()
     {
