f22da492bc17089518e5c6322a1e10ca678649ac
dbaeka <dbaekarjnr@gmail.com>
2023-03-11 19:51:52 +0000

    :construction: update root html to use laravel mix

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -4,12 +4,16 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+        <title>Checkout</title>
+
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('/css/plugins.css') }}" rel="stylesheet">
     </head>
     <body class="bg-gray-50">
         <div class="mx-auto max-w-2xl px-4 pt-16 pb-24 sm:px-6 lg:max-w-7xl lg:px-8" id="app">
             <checkout></checkout>
         </div>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
